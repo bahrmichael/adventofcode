9f710ca91c63334e01a5f6744b41aa8f0f7c4fad
bahrmichael <mbahr92@gmail.com>
2017-12-06 12:50:19 +0100

    Day6.1

---
+++ src/main/java/Day6.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Day6 {
+
+    static int solve1(final CharSequence text) {
+        return solve(text);
+    }
+
+    private static int solve(final CharSequence text) {
+        final Collection<String> seenConfigurations = new HashSet<>();
+        int[] array = Util.extractNumbers(text, "\t");
+
+        while (true) {
+            final String str = Arrays.toString(array);
+            if (seenConfigurations.contains(str)) {
+                return seenConfigurations.size();
+            }
+
+            array = redistribute(array);
+
+            seenConfigurations.add(str);
+        }
+    }
+
+    static int[] redistribute(final int[] array) {
+        int index = getClearanceIndex(array);
+        int distributeValue = array[index];
+        array[index] = 0;
+        while (distributeValue > 0) {
+            index++;
+            array[index % array.length] += 1;
+            distributeValue--;
+        }
+        return array;
+    }
+
+    static int getClearanceIndex(final int[] array) {
+        int maxIndex = 0;
+        int maxValue = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    static final String PUZZLE = "14\t0\t15\t12\t11\t11\t3\t5\t1\t6\t8\t4\t9\t1\t8\t4";
+}
---
+++ src/test/java/Day6Test.java
@@ -0,0 +1,48 @@
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day6Test {
+
+    @Test
+    void solve() {
+        System.out.println("1: " + Day6.solve1(Day6.PUZZLE));
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider")
+    void redistribute(final int[] in, final int[] expected) {
+        assertArrayEquals(expected, Day6.redistribute(in));
+    }
+
+    private static Stream<Arguments> arrayProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 2, 7, 0}, new int[]{2, 4, 1, 2}),
+                Arguments.of(new int[]{2, 4, 1, 2}, new int[]{3, 1, 2, 3}),
+                Arguments.of(new int[]{3, 1, 2, 3}, new int[]{0, 2, 3, 4}),
+                Arguments.of(new int[]{0, 2, 3, 4}, new int[]{1, 3, 4, 1}),
+                Arguments.of(new int[]{1, 3, 4, 1}, new int[]{2, 4, 1, 2})
+                        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayForClearanceProvider")
+    void getClearanceIndex(final int[] in, final int expectedIndex) {
+        assertEquals(expectedIndex, Day6.getClearanceIndex(in));
+    }
+
+    private static Stream<Arguments> arrayForClearanceProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 2, 7, 0}, 2),
+                Arguments.of(new int[]{2, 4, 1, 2}, 1),
+                Arguments.of(new int[]{3, 1, 2, 3}, 0),
+                Arguments.of(new int[]{0, 2, 3, 4}, 3),
+                Arguments.of(new int[]{1, 3, 4, 1}, 2)
+                        );
+    }
+}
